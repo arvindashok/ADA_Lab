8508b19df5cd24ed702b7cd7641633a5e2d87b52
Arvind Ashok <117076503+arvindashok@users.noreply.github.com>
2023-08-31 21:11:02 +0530

    Add files via upload

--- flights_dijkstra.c
+++ flights_dijkstra.c
@@ -8,10 +8,11 @@ int min (int dist[n], int flag[n]){
     int min = INT_MAX/2, index;
     for (int i=0; i<n; i++){
         if (flag[i]==0 && dist[i]<min){
-            min=i;
+            min=dist[i];
+            index=i;
         }
     }
-    return min;
+    return index;
 }
 
 void dijkstra (int graph[n][n], int src, int dest){
@@ -27,17 +28,17 @@ void dijkstra (int graph[n][n], int src, int dest){
     for (i=0; i<n; i++){
         int u = min (dist, flag);
 
-        flag[i]=1;
+        flag[u]=1;
         for (int v=0; v<n; v++){
             if (flag[v]==0 && graph[u][v] && dist[u]+graph[u][v] < dist[v])
                 dist[v] = dist[u]+graph[u][v];
         }
     }
-    for (i=0; i<n; i++){
-        if (dist[i]==INT_MAX/2){
-            dist[i]=2;
-        }
-    }
+    // for (i=0; i<n; i++){
+    //     if (dist[i]==INT_MAX/2){
+    //         dist[i]=2;
+    //     }
+    // }
     printf ("Vertex  Distance\n");
     for (i=0; i<n; i++){
         // if (i==dest)
@@ -67,7 +68,7 @@ int main(){
     printf("0: Ahmedabad\n1: Amritsar\n2: Bengaluru\n3: Chandigarh\n4: Chennai\n5: Delhi\n6: Hyderabad\n7: Jaipur\n8: Kochi\n9: Kolkata\n10: Mumbai\n11: Nagpur\n12: Pune\n13: Srinagar\n");
     printf ("\nEnter Departure:\n");
     scanf ("%d", &src);
-    printf ("0: Ahmedabad\n1: Amritsar\n2: Bengaluru\n3: Chandigarh\n4: Chennai\n5: Delhi\n6: Hyderabad\n7: Jaipur\n8: Kochi\n9: Kolkata\n10: Mumbai\n11: Nagpur\n12: Pune\n13: Srinagar\n");
+    printf("\n0: Ahmedabad\n1: Amritsar\n2: Bengaluru\n3: Chandigarh\n4: Chennai\n5: Delhi\n6: Hyderabad\n7: Jaipur\n8: Kochi\n9: Kolkata\n10: Mumbai\n11: Nagpur\n12: Pune\n13: Srinagar\n");
     printf ("\nEnter Arrival:\n");
     scanf ("%d", &dest);
     dijkstra (graph, src, dest);
